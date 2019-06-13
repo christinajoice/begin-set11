f1e527721927b9edd1d846496d13fca1eb827541
christinajoice <christinallrejoice@gmail.com>
2019-06-13 11:42:41 +0530

    Update kthsmall.py

--- kthsmall.py
+++ kthsmall.py
@@ -1,8 +1,4 @@
-n=int(input("Enter number of values"))
-k=int(input("Enter the order of smallest number"))
-print("Enter the values")
-li=[]
-for i in range(0,n):
-  li.append(int(input()))
+n,k=map(int,input().split())
+li=list(map(int,input().split()))
 li.sort()
 print(li[k-1])
