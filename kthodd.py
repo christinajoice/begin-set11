6b5e60150eb475410466b6d87c3e409be5fabef4
christinajoice <christinallrejoice@gmail.com>
2019-06-13 11:34:47 +0530

    Update kthodd.py

--- kthodd.py
+++ kthodd.py
@@ -1,9 +1,7 @@
-n=int(input("Enter number of elements"))
-k=int(input("Enter the order of odd number"))
+n,k=map(int,input().split())
+lis=list(map(int,input().split()))
 li=[]
-print("Enter values")
-for i in range(0,n):
-  num=int(input())
-  if num%2!=0:
-    li.append(num)
+for i in lis:
+    if i%2!=0:
+        li.append(i)
 print(li[k-1])
