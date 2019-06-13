b0670e4f629122207c585f8aa9ef65cfecdd6b86
christinajoice <christinallrejoice@gmail.com>
2019-06-13 11:02:48 +0530

    Update uppercase.py

--- uppercase.py
+++ uppercase.py
@@ -1,6 +1,6 @@
-str1=input("Enter a string")
+str1=input()
 str1[1].upper()
-for i in range(len(str1)):
-  if str1[i]==' ':
-    str[i+1].upper()
+for i in str1:
+  if i==' ':
+    i+1.upper()
 print(str1)
